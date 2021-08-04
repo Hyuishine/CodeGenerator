6ad8cd99c778dd75cc1697a9080a5c1d1acb91e6
hyuishine <hyuishine@gmail.com>
2021-08-04 15:52:24 +0800

    分离开发文档生成模块的首页，分离成各个组件。

--- src/view/doc-generator/index.vue
+++ src/view/doc-generator/index.vue
@@ -3,93 +3,83 @@
  * @Author: 黄宇/hyuishine
  * @Date: 2021-08-01 14:24:38
  * @LastEditors: 黄宇/Hyuishine
- * @LastEditTime: 2021-08-01 17:54:23
+ * @LastEditTime: 2021-08-04 15:48:54
  * @Description:
  * @Email: hyuishine@gmail.com
  * @Company: 3xData
  * @youWant: add you want
 -->
 <template>
-  <div>
-    <v-file-input show-size
-                  label="上传开发文档"
-                  @change="fileUpload"></v-file-input>
-    <div v-html="htmlData"></div>
-  </div>
+  <v-card>
+
+    <!-- 步骤标题 -->
+    <v-card-title class="text-h6 font-weight-regular justify-space-between">
+      <span>{{ stepData.title[currentStep - 1] }}</span>
+    </v-card-title>
+
+    <!-- 步骤 动态组件 -->
+    <v-window v-model="currentStep">
+      <component :is="path"
+                 v-for="(path,i) in   stepData.path"
+                 :key="i"></component>
+    </v-window>
+
+    <!-- 分割线 -->
+    <v-divider></v-divider>
+
+    <!-- 操作栏，操作按钮 -->
+    <v-card-actions>
+      <v-btn :disabled="currentStep === 1"
+             text
+             @click="currentStep--">
+        上一步
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn :disabled="currentStep === 3"
+             color="primary"
+             depressed
+             @click="currentStep++">
+        下一步
+      </v-btn>
+    </v-card-actions>
+
+  </v-card>
 </template>
+
 <script>
-import $ from 'jquery'
-import './files/mammoth.browser.js'
+
+const moduleObj = {} // 储存引入的组件对象 用于 注册组件
+const path = require('path') // 地址类型
+const files = require.context('./step', true, /\.vue$/) // 正则遍历找出"./components"中的“ .vue ” 结尾的文件
+files.keys().forEach(key => { // 将path 对象变为能用的对象
+  const name = path.basename(key, '.vue') // 将获取到的文件名 去除后缀，得到该模块的名字
+  moduleObj[name] = files(key).default || files(key) // 将文件对象 以上面得到的该模块的名字 为key值 存为组件对象，用于注册组件
+})
+
 export default {
+  name: 'pageWord',
   data () {
     return {
-      htmlData: ''
+      currentStep: 1,
+      stepData: {
+        title: [], // 标题数据
+        path: [] // 动态组件可以：is使用的 路径
+      }
     }
   },
-  methods: {
-    //   上传文件事件，先转buffer，然后再转html
-    fileUpload (file) {
-      if (file != null) {
-        let that = this
-        // 将上传的文件，转换为buffer然后再转给html，然后再整理出表单、列表、列表的搜索字段
-        this.toBuffer(file, function (arrayBuffer) {
-          mammoth.convertToHtml({ arrayBuffer: arrayBuffer })
-            .then(res => {
-              // 渲染转化成html的开发文档
-              that.htmlData = res.value
-              // 整理转化后的开发文档
-              setTimeout(() => {
-                that.sortHtml(res)
-              }, 1000)
-            })
-            .done()
-        })
-      } else {
-        this.htmlData = '暂无数据'
-      }
-    },
-    // 将文件格式转换为 arrayBuffer
-    toBuffer (file, callback) {
-      var reader = new FileReader()
+  created () {
+    let step = ['docUpload']
 
-      reader.onload = (loadEvent) => {
-        var arrayBuffer = loadEvent.target.result
-        callback(arrayBuffer)
+    step.forEach(stepName => {
+      // 当步骤库中存在定义的步骤，则按定义的步骤顺序存放步骤组件
+      if (Object.keys(moduleObj).indexOf(stepName) !== -1) {
+        // 获取组件对象中的cname值（name值用中文似乎会报错） 作为步骤的标题值
+        this.stepData.title.push(moduleObj[stepName].cname)
+        this.stepData.path.push(moduleObj[stepName])
       }
-
-      reader.readAsArrayBuffer(file)
-    },
-    // html 特殊符号兼容性替换
-    escapeHtml (value) {
-      return value
-        .replace(/&/g, '&amp;')
-        .replace(/"/g, '&quot;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-    },
-    // 整理转换后的html
-    sortHtml (html) {
-      let formData = {
-        modelName: '', // 模块名
-        permission: '', // 权限
-        items: [
-          {
-            zhName: '', // 字段中文名
-            type: '', // 字段类型
-            other: ''
-          }
-        ]
-      }
-      $('p').map((i, ele) => {
-        if (ele.innerHTML.indexOf('$') !== -1) {
-          console.log(ele)
-          // if (ele.innerHTML.indexOf('【表单信息】') !== -1) {
-          //   // 表单模块名
-          //   console.log($(ele).next().find('strong')[0].innerText)
-          // }
-        }
-      })
-    }
+    })
   }
 }
 </script>
