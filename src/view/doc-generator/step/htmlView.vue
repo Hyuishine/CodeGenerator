bbb26f952532a94af5c6efcddd5a2ffe6d32181d
hyuishine <hyuishine@gmail.com>
2021-08-05 15:00:23 +0800

    添加生成后预览步骤组件

---
+++ src/view/doc-generator/step/htmlView.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author: 黄宇/Hyuishine
+ * @Date: 2021-08-04 16:10:38
+ * @LastEditors: 黄宇/Hyuishine
+ * @LastEditTime: 2021-08-05 14:37:04
+ * @Description:
+ * @Email: hyuishine@gmail.com
+ * @Company: 3xData
+ * @youWant: add you want
+-->
+<template>
+  <v-window-item :value="2">
+      <v-card-text v-html="htmlData"
+                   id="htmlViewContainer">
+      </v-card-text>
+  </v-window-item>
+</template>
+<script>
+export default {
+  name: 'htmlView',
+  cname: '开发文档，转换预览',
+  computed: {
+    htmlData () {
+      return this.$store.state.module.htmlDom
+    }
+  }
+}
+</script>
